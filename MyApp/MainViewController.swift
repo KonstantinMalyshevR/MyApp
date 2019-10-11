033abc9c37fcb7d61cccbad5af4b1041156f9c24
Konstantin Malyshev <rmkonstantin@gmail.com>
2019-10-11 18:23:10 +0900

    fix

--- MyApp/MainViewController.swift
+++ MyApp/MainViewController.swift
@@ -40,6 +40,15 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         newsTableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
         newsTableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8).isActive = true
 
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 80))
+        footerView.backgroundColor = .clear
+        let progress = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        progress.startAnimating()
+        progress.color = .red
+        footerView.addSubview(progress)
+        progress.center = CGPoint(x: footerView.frame.size.width / 2, y: footerView.frame.size.height / 2)
+        newsTableView.tableFooterView = footerView
+
         loadData()
     }
 
@@ -53,8 +62,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
     //MARK: - TableView
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cell.contentView.layer.masksToBounds = true
-        if indexPath.row + 1 == newsArray.count {
+        if indexPath.row == newsArray.count - 1 {
             loadData()
         }
     }
@@ -66,6 +74,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.heightAnchor.constraint(equalToConstant: 80.0).isActive = true
+        cell.backgroundColor = .clear
         cell.textLabel?.backgroundColor = UIColor.clear
         cell.textLabel?.text = newsArray[indexPath.row].title
         cell.textLabel?.textColor = UIColor.black
